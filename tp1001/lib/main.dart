b8fd9e803d839f97c4f8f94e55ed6c01e74068a7
Rachit <rachitplah@outlook.com>
2019-04-10 16:48:33 +0530

    New Start

--- tp1001/lib/main.dart
+++ tp1001/lib/main.dart
@@ -1,15 +1,8 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
+// Copyright 2019 REPA Authors. All rights reserved.
+// Use of this source code is governed by REPA developers license DEV1MA that can be
 // found in the LICENSE file.
-
-// You can read about packages here: https://flutter.io/using-packages/
 import 'package:flutter/material.dart';
-
-// You can use a relative import, i.e. `import 'category.dart;'` or
-// a package import, as shown below.
-// More details at http://dart-lang.github.io/linter/lints/avoid_relative_lib_imports.html
 import 'package:tp1001/mainScreen.dart';
-
 
 /// The function that is called when main.dart is run.
 void main()=>runApp(tp101App());

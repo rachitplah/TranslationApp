c63bdcebfa273825391b9628e214438b159bb0f7
Rachit <rachitplah@outlook.com>
2019-05-14 07:21:40 +0530

    added save button

--- tp1001/lib/editScreen.dart
+++ tp1001/lib/editScreen.dart
@@ -13,13 +13,16 @@ class editScreen extends StatelessWidget{
         title: Text('Edit Translation'),
       ),
       body: Material(
-        child: Padding(
+        child: 
+        Column(
+          children: <Widget>[
+Padding(
               padding: EdgeInsets.only(left:10.0,right:10.0,bottom: 10.0,top:30.0),
               child:
             TextField(
               style: TextStyle(color: Colors.blue[400]),
               onSubmitted: (String userInput) {
-                         moveToLastScreen(context);
+                         //moveToLastScreen(context);
               },
               decoration: InputDecoration(
                 labelText: 'Enter translation here',
@@ -30,6 +33,31 @@ class editScreen extends StatelessWidget{
               ),
             ),
             ),
+            Padding(
+                   padding: EdgeInsets.only(top:30,left:20,right:20),
+                   child:
+                   Container(
+                     child: RaisedButton(
+                               child: 
+                                    Text(
+                                         "SAVE",
+                                        style: TextStyle(color: Colors.white),
+                                      ),                          
+                              color: Colors.blue[400],
+                              padding: EdgeInsets.all(0),
+                              elevation: 10.0,
+                              shape: RoundedRectangleBorder(
+                                     borderRadius: BorderRadius.circular(5.0)),
+                              onPressed: (){
+                                moveToLastScreen(context);
+                              },
+                           ),
+                      // width: 50.0,
+                       height: 50.0,
+                   ),
+                 ),
+          ],
+        ),
       ),
     ),
     );

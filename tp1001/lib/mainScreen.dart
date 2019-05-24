504cc915c02fcf569050f86e2c6d4b3e25bed2ff
Rachit <rachitplah@outlook.com>
2019-05-24 20:22:35 +0530

    Some changes

--- tp1001/lib/mainScreen.dart
+++ tp1001/lib/mainScreen.dart
@@ -788,3 +788,77 @@ class resultListState2 extends State<resultList2> with AutomaticKeepAliveClientM
     });
   }
 }
+
+class FirebaseDemoScreen extends StatelessWidget {
+
+  final databaseReference = FirebaseDatabase.instance.reference();
+  /*
+  @override
+  Widget build(BuildContext context) {
+    getData();
+    return Scaffold(
+        appBar: AppBar(
+            title: Text('Firebase Connect'),
+            ),
+        body: Center(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+
+                  RaisedButton(
+                      child: Text('Create Record'),
+                      onPressed: () {
+                        createRecord();
+                      },
+                  ),
+
+                  RaisedButton(
+                      child: Text('View Record'),
+                      onPressed: () {
+                        getData();
+                      },
+                  ),
+                  RaisedButton(
+                      child: Text('Udate Record'),
+                      onPressed: () {
+                        updateData();
+                      },
+                  ),
+                  RaisedButton(
+                      child: Text('Delete Record'),
+                      onPressed: () {
+                        deleteData();
+                      },
+                  ),
+                ],
+            )
+        ), //center
+    );
+  }
+  */
+  void createRecord(){
+    databaseReference.child("1").set({
+      'title': 'Mastering EJB',
+      'description': 'Programming Guide for J2EE'
+    });
+    databaseReference.child("2").set({
+      'title': 'Flutter in Action',
+      'description': 'Complete Programming Guide to learn Flutter'
+    });
+  }
+  void getData(){
+    databaseReference.once().then((DataSnapshot snapshot) {
+      print('Data : ${snapshot.value}');
+    });
+  }
+
+  void updateData(){
+    databaseReference.child('1').update({
+      'description': 'J2EE complete Reference'
+    });
+  }
+
+  void deleteData(){
+    databaseReference.child('1').remove();
+  }
+}

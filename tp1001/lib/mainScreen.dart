43a7f1151a1d5803390d755ba5d50fa21dc1127c
Rachit <rachitplah@outlook.com>
2019-05-24 22:38:07 +0530

    added firebase helper

--- tp1001/lib/mainScreen.dart
+++ tp1001/lib/mainScreen.dart
@@ -8,7 +8,7 @@ import 'package:tp1001/profileScreen.dart';
 import 'package:tp1001/api.dart';
 import 'package:translator/translator.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:firebase_database/firebase_database.dart';
+import 'package:tp1001/utils/firebase_helper1.dart';
 var l1,l2;
 var emoti,exists=0;
 String results="";
@@ -113,6 +113,7 @@ class mScreenState extends State<mScreen> with AutomaticKeepAliveClientMixin{
                    print(userInput);
                    print(l1);
                      print(l2);
+                     FirebaseDemoScreen().createRecord();
                   convert1(userInput,l1,l2,context,data);
                    //print('This is $results');
 
@@ -786,79 +787,5 @@ class resultListState2 extends State<resultList2> with AutomaticKeepAliveClientM
             });
         });
     });
-  }
-}
-
-class FirebaseDemoScreen extends StatelessWidget {
-
-  final databaseReference = FirebaseDatabase.instance.reference();
-  /*
-  @override
-  Widget build(BuildContext context) {
-    getData();
-    return Scaffold(
-        appBar: AppBar(
-            title: Text('Firebase Connect'),
-            ),
-        body: Center(
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-
-                  RaisedButton(
-                      child: Text('Create Record'),
-                      onPressed: () {
-                        createRecord();
-                      },
-                  ),
-
-                  RaisedButton(
-                      child: Text('View Record'),
-                      onPressed: () {
-                        getData();
-                      },
-                  ),
-                  RaisedButton(
-                      child: Text('Udate Record'),
-                      onPressed: () {
-                        updateData();
-                      },
-                  ),
-                  RaisedButton(
-                      child: Text('Delete Record'),
-                      onPressed: () {
-                        deleteData();
-                      },
-                  ),
-                ],
-            )
-        ), //center
-    );
-  }
-  */
-  void createRecord(){
-    databaseReference.child("1").set({
-      'title': 'Mastering EJB',
-      'description': 'Programming Guide for J2EE'
-    });
-    databaseReference.child("2").set({
-      'title': 'Flutter in Action',
-      'description': 'Complete Programming Guide to learn Flutter'
-    });
-  }
-  void getData(){
-    databaseReference.once().then((DataSnapshot snapshot) {
-      print('Data : ${snapshot.value}');
-    });
-  }
-
-  void updateData(){
-    databaseReference.child('1').update({
-      'description': 'J2EE complete Reference'
-    });
-  }
-
-  void deleteData(){
-    databaseReference.child('1').remove();
   }
 }
